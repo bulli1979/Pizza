1f16c4a28a7713b65b2b6ebf5470fb1fa4b36fc8
fabricio-ruch <ruch_fabricio@hotmail.com>
2017-10-27 19:10:34 +0200

    SceneManager

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,48 +1,27 @@
 package application;
-	
-import java.util.HashMap;
-import java.util.Map;
+
 import javafx.application.Application;
-import javafx.geometry.Rectangle2D;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 import order.OrderData;
-import scenes.HomeScene;
-import scenes.PizzaScene;
-import javafx.scene.layout.BorderPane;
-
+import scenemanagement.SceneManager;
 
 public class Main extends Application {
-	Map<String, PizzaScene> scenes;
-	Stage primaryStage;
+
+	private SceneManager sceneManager;
+	
 	@Override
 	public void start(Stage primaryStage) {
+		
+		this.sceneManager = new SceneManager(this, primaryStage, new OrderData());
 		try {
-			this.primaryStage = primaryStage;
-			Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-			OrderData orderData = new OrderData();
-			BorderPane root = new BorderPane();
-			scenes = new HashMap<String,PizzaScene>();
-			double width = visualBounds.getWidth();
-			double height = visualBounds.getHeight();
-			HomeScene homeScene = new HomeScene.Builder().setRoot(root).withWidth(width).withHeight(height).data(orderData).application(this).build();
-			homeScene.initialize();
-			scenes.put("home",homeScene);
 			
-			primaryStage.setFullScreen(true);
-			setScene("home");
-			primaryStage.show();
-			
+			sceneManager.setScene("home");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void setScene(String sceneName){
-		primaryStage.setScene(scenes.get(sceneName).getScene());
-	}
-	
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
---
+++ src/scenemanagement/SceneManager.java
@@ -0,0 +1,56 @@
+package scenemanagement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import application.Main;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import order.OrderData;
+import scenes.HomeScene;
+import scenes.PizzaScene;
+
+public class SceneManager {
+	private Map<String, PizzaScene> scenes;
+	private Stage primaryStage;
+	private BorderPane root;
+	private double width;
+	private double height;
+	private Main application;
+	private OrderData orderData;
+	
+	public SceneManager(Main application, Stage primaryStage, OrderData orderData)
+	{
+		this.application = application;
+		this.orderData = orderData;
+		this.primaryStage = primaryStage;
+		Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+		root = new BorderPane();
+		scenes = new HashMap<String,PizzaScene>();
+		width = visualBounds.getWidth();
+		height = visualBounds.getHeight();
+		
+		initScenes();
+		
+		primaryStage.setFullScreen(true);
+		primaryStage.show();
+	}
+	
+	private void initScenes() {
+		initHomeScene();
+	}
+
+	private void initHomeScene()
+	{
+		HomeScene homeScene = new HomeScene.Builder().setRoot(root).withWidth(width).withHeight(height).data(orderData).application(application).build();
+		homeScene.initialize();
+		scenes.put("home",homeScene);
+	}
+	
+	public void setScene(String sceneName){
+		primaryStage.setScene(scenes.get(sceneName).getScene());
+	}
+	
+}
